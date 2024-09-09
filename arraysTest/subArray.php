91cf7efdd19a7917e5b43fb5212210e99bf597c8
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-09-09 15:37:37 -0300

    Create subArray.php

---
+++ arraysTest/subArray.php
@@ -0,0 +1,7 @@
+$meuArray = [
+    10,               // Valor simples
+    20,               // Valor simples
+    [1, 2, 3],        // Subvetor
+    [4, 5, 6],        // Subvetor
+    30                // Valor simples
+];
